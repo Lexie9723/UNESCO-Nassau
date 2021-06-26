d99ef88751637b9adc109dcd02a67d68574788c1
Lexie9723 <1351581045@qq.com>
2021-06-26 17:42:13 +0800

    Nassau

---
+++ js/manifest.js
@@ -0,0 +1,15 @@
+(function () {  
+
+_mrmcp = (typeof _mrmcp == 'undefined') ? {} : _mrmcp;
+_mrmcp['campaign_id'] = 'none';
+_mrmcp['owner_id'] = '5d6b6a4ecec589741d5cfc7b';
+_mrmcp['creative_id'] = '5dfb27e26b03e6349f219270';
+_mrmcp['ga_url'] = _mrmcp['ga_url'] || (('https:' == document.location.protocol ? 'https://' : 'http://') + 'card.mugeda.com/weixin/card/ga.js');
+_mrmcp['width'] = _mrmcp['width'] || 320;
+_mrmcp['height'] = _mrmcp['height'] || 520;
+_mrmcp['type'] = 'smart';
+_mrmcp['title'] = 'UNESCO&Nassau';
+_mrmcp['build_number'] = parseInt('1856');
+_mrmcp['publish_time'] = 'Sun Dec 29 2019 13:13:38 GMT+0800 (CST)';
+_mrmcp['track_bot'] = 'http://cdn.mugeda.com/media/pages/track/track_20131030.html';
+})();
